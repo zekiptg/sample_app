ffb9e62c5fc44c05fef91ad067a3123269e1ff18
nguyen.quang.huyd <FRAMGIA\nguyen.quang.huyd@b121623-pc.framgia.com>
2018-12-05 08:15:04 +0700

    Chapter 3: Mostly static pages

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def hello
+    render html: "hello, world!"
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+end
