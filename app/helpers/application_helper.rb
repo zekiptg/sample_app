5ce2f74503fc318dfcd93ccf2c216b174656f858
nguyen.quang.huyd <FRAMGIA\nguyen.quang.huyd@b121623-pc.framgia.com>
2018-12-06 09:18:29 +0700

    Chapter 4: Rails-flavored Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis. # Documentation comment
+  def full_title page_title
+    return t("title") if page_title.blank?
+    page_title + " |" + t("title")
+  end
 end
